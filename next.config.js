f9cb8b6501b75623ffa4ad37d21217600e0e1d94
Mark Baggett <mbagget1@utk.edu>
2022-06-24 09:34:07 -0400

    Attempt CORS fix.

--- next.config.js
+++ next.config.js
@@ -25,3 +25,20 @@ module.exports = (phase) => {
   canopy.buildCanopy(env);
   return { env };
 };
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // match all Gotham routes
+        source: "/public/fonts/Gotham/*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
+};
