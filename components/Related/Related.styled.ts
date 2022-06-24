95a8cd8b9e65a4ce60d5236f732f154c60a8f63e
Mark Baggett <mbagget1@utk.edu>
2022-06-24 11:36:59 -0400

    Clean up More Like. :broom:

--- components/Related/Related.styled.ts
+++ components/Related/Related.styled.ts
@@ -1,12 +1,16 @@
 import { styled } from "@/stiches.config";
 
-const RelatedWrapper = styled("div", { padding: "0 1.618rem 2rem" });
-
-const StyledRelated = styled("section", {
+const RelatedWrapper = styled("div", {
+  padding: "0 1.618rem 2rem",
   maxWidth: "1280px",
   margin: "auto",
   position: "relative",
-  padding: "1rem 0 0",
+});
+
+const StyledRelated = styled("section", {
+  backgroundColor: "$gray1",
+  paddingTop: "1rem",
+  boxShadow: "inset 0 1px 1px rgba(0, 0, 0, 0.075);",
 });
 
 export { StyledRelated, RelatedWrapper };
