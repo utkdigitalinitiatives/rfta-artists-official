c3e25dab47bd028bbed57c06799290b80e854b9d
Mark Baggett <mbagget1@utk.edu>
2022-07-09 11:10:50 -0500

    Add thumbnails to metatags.

--- pages/works/[slug].tsx
+++ pages/works/[slug].tsx
@@ -3,20 +3,20 @@ import { client } from "@/pages/api/graphql";
 import Layout from "@/components/layout";
 import Viewer from "@/components/Viewer/Viewer";
 import { Vault } from "@iiif/vault";
-import Metatag from "@/components/Metatag/Metatag"
-
+import Metatag from "@/components/Metatag/Metatag";
+import { Summary, Thumbnail } from "@samvera/nectar-iiif";
 import Related from "@/components/Related/Related";
 import WorkInner from "@/components/Work/Inner";
 
 export default function Manifest({ manifest }) {
-  const { id, label, metadata } = manifest;
+  const { id, label, metadata, summary, thumbnail } = manifest;
   const artist = metadata.filter(function (x) {
     if(x.label.en[0] == "Artist" ){return x.value.en[0]}
   } )[0].value.en[0];
 
   return (
     <Layout>
-      <Metatag label={label} summary={summary} />
+      <Metatag label={label} summary={summary} thumbnail={thumbnail}/>
       <div style={{ padding: "1.31rem 0 0" }}>
         <Viewer manifestId={id} />
       </div>
