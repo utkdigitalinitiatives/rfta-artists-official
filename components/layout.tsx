26e00735f81a65f64d4728587ce837b1ab8bd0c8
Mark Baggett <mbagget1@utk.edu>
2022-07-12 11:50:30 -0500

    Init GA.

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Header from "@/components/Header/Header";
 import UTKHeader from "@/components/UTK_Header/UTKHeader"
 import Footer from "@/components/Footer/Footer";
+import Script from 'next/script'
 
 export const siteTitle = "Next.js Sample Website";
 
@@ -16,6 +17,19 @@ export default function Layout({ children }) {
       <Header />
       <main>{children}</main>
       <Footer />
+      <div className="container">
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-5931387-33', 'auto');
+          ga('send', 'pageview');
+        `}
+        </Script>
+      </div>
     </>
   );
 }
