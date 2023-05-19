fc71b4bf91f1f1347d6484f9d10c2e42491a81ed
Mark Baggett <mbagget1@utk.edu>
2023-05-19 13:34:58 -0400

    Init switch to GTM / GA4.

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,12 +5,24 @@ import Footer from "@/components/Footer/Footer";
 import Script from 'next/script'
 import { useRouter } from "next/router";
 import { NextSeo } from 'next-seo';
+import Head fomr 'next/head';
 
 export default function Layout({ children }) {
   const router = useRouter();
   const canonicalUrl = (`https://rfta-artists.lib.utk.edu` + (router.asPath === "/" ? "": router.asPath)).split("?")[0];
   return (
     <>
+      <Head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DN6TP2L65T"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-DN6TP2L65T');
+        </script>
+      </Head>
       <NextSeo
         canonical={canonicalUrl}
         titleTemplate='RFTA: The Chimney Tops 2 Wildfires in Memory and Art | %s'
@@ -43,19 +55,6 @@ export default function Layout({ children }) {
       <Header />
       <main>{children}</main>
       <Footer />
-      <div className="container">
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'UA-5931387-33', 'auto');
-          ga('send', 'pageview');
-        `}
-        </Script>
-      </div>
     </>
   );
 }
