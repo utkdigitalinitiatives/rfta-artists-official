e0ccb9a0d3af342adea6e7f79e741e9ed5d181aa
David Shaw <dshaw11@utk.edu>
2025-12-01 11:34:28 -0500

    more build adjustments

--- scripts/canopy-build.js
+++ scripts/canopy-build.js
@@ -2,8 +2,8 @@ const config = require("../canopy.config");
 const canopy = require("../services/canopy");
 
 const env = {
-  ...config.globals,
-  ...config.prod,
+    ...config.globals,
+    ...config.prod,
 };
 
 console.log("Building Canopy data files...");
@@ -11,6 +11,6 @@ canopy.buildCanopy(env);
 
 // Give async operations time to complete
 setTimeout(() => {
-  console.log("Canopy build complete!");
-  process.exit(0);
+    console.log("Canopy build complete!");
+    process.exit(0);
 }, 10000);
